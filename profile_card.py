818efbc2581ea9c599745e05809a77d58b043351
fezyinya-boop <fezyinya@gmail.com>
2026-03-07 21:44:47 -0600

    Update profile_card.py

--- profile_card.py
+++ profile_card.py
@@ -548,7 +548,7 @@ def make_profile_card(
     border_overlay = Image.new("RGBA", (W, H), (0, 0, 0, 0))
     bd = ImageDraw.Draw(border_overlay)
 
-    inset = S(3)
+    inset = S(2)
 
     # Outer gold frame
     bd.rounded_rectangle(
